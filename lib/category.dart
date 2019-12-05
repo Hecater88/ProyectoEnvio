2a4ea6380f28d58cfcea002361c8056df99145b3
HecaTer <HecaTer@users.noreply.github.com>
2019-12-05 13:35:21 +0100

    First Commit

---
+++ lib/category.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:category_widget/unit.dart';
+import 'package:meta/meta.dart';
+
+
+//Custom widget llamado Category
+class Category {
+  final String name;
+  final ColorSwatch color;
+  final IconData iconLocation;
+  final List<Unit> units;
+
+  const Category({
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+    @required this.units,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        assert(units != null);
+        
+/*
+  //Navigates to the Converter Route
+  void _navigateToConverter(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<Null>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+            title: Text(
+              name,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            centerTitle: true,
+            backgroundColor: color,
+          ),
+          body: ConverterRoute(
+            color: color,
+            units: units,
+          ),
+        );
+      },
+    ));
+  }
+
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: color,
+          splashColor: color,
+          onTap: () => _navigateToConverter(context),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 16.0),
+                  child: Icon(
+                    iconLocation,
+                    size: 60.0,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  */
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:category_widget/category_route.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(UnitConverterApp());
+
+class UnitConverterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title: 'Unit Converter',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.black,
+          displayColor:Colors.grey[600],
+        ),
+
+        primaryColor:Colors.grey[500],
+        textSelectionHandleColor:Colors.green[500],
+      ),
+      home: CategoryRoute(),
+    );
+  }
+}
